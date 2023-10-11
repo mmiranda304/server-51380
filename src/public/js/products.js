37c1bf69661628896e7bbd8a118e3a4ec98b4384
Manuel Miranda <manuel.miranda304@gmail.com>
2023-10-10 22:29:53 -0300

    fixes at public/js - fetch routes

--- src/public/js/products.js
+++ src/public/js/products.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:8080/api";
+const API_URL = "/api";
 const cartIdElement = document.getElementsByClassName('cartID')[0];
 
 const addToCart = (_id) => {
--- src/public/js/realTimeProducts.js
+++ src/public/js/realTimeProducts.js
@@ -20,45 +20,6 @@ socket.on('newProduct', (products) => {
 
 });
 
-// const form = document.querySelector('form'); 
-// form.addEventListener('submit', e => {
-//     e.preventDefault();
-
-//     const title         = document.getElementById('title').value;
-//     const description   = document.getElementById('description').value;
-//     const price         = document.getElementById('price').value;
-//     const thumbnail     = document.getElementById('thumbnail').value;
-//     const code          = document.getElementById('code').value;
-//     const stock         = document.getElementById('stock').value;
-//     const status        = document.getElementById('status').checked;
-
-//     if(!title || !description || !price || !thumbnail || !code || !stock) {
-//         alert('Todos los campos son obligatorios');
-//         return;
-//       }
-//     if(!/^https?:\/\//i.test(thumbnail)) {
-//     alert('La URL de la imagen no es válida');
-//     return;
-//     }
-//     if(isNaN(stock) || stock < 0) {
-//         alert('El stock debe ser un número mayor o igual a cero');
-//         return;
-//     }
-//     const product = {
-//         title,
-//         description,
-//         price: parseFloat(price),
-//         thumbnail,
-//         code,
-//         stock: parseInt(stock),
-//         status
-//       };
-    
-//       socket.emit('newProduct', product);
-//       form.reset();
-//       alert('El producto ha sido agregado correctamente'); 
-// });
-
 function deleteProduct(_id) {
     socket.emit('deleteProduct', _id);
 }
